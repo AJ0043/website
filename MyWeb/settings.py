d3650d7c68d95afbbb79a47235db28a5e454190b
Anvesh Jain <anveshjain@Anveshs-MacBook-Air.local>
2024-12-31 12:35:28 +0530

    first commit

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)8x#x+9t5a7%48)p@kl%jr&8ti51kx4&lvqczx&)0x@h*=qo%=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
